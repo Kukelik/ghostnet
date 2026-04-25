6dc7741699b76f0677da58561e62ba4267581c52
Kukelik <kuklikfabian@gmail.com>
2026-04-25 20:58:25 +0200

    Initial commit: Spring Boot project setup

---
+++ src/main/java/de/ghostnet/ghostnet/GhostnetApplication.java
@@ -0,0 +1,13 @@
+package de.ghostnet.ghostnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhostnetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhostnetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/ghostnet/ghostnet/GhostnetApplicationTests.java
@@ -0,0 +1,13 @@
+package de.ghostnet.ghostnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhostnetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
